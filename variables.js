8ed5f3f752314df2fabf60ad412a1aa538a8dc16
Keerthichandra Chikka <52312861+KeerthichandraChikka@users.noreply.github.com>
2024-12-05 17:34:55 +0000

    first commit

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 10101011;
+let email = 'Hello@gmail.com';
+let name1 = "Keerthichandra";
+ city = "Hyderabad";
+
+//accountId = 123;
+
+email = "hi@gmail.com";
+name1 = "Kirthichandra";
+city = "Pune";
+
+console.log(accountId);
+console.table([email, name1, city]);
