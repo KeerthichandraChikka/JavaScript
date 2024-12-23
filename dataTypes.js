62d6b1344efb04f09be3f9d0a2eeb4616857adea
Keerthichandra Chikka <52312861+KeerthichandraChikka@users.noreply.github.com>
2024-12-23 11:37:38 +0000

    data types

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; 
+let name = "Keerthichandra";
+let age = 30;
+let isLoggedIn = false;
+
+//Primitive data types:
+
+// number range is 2^52 and 
+// bigInt range
+// String '' or ""
+// boolean false or true
+// null -> stand alone value (representation of null)
+//undefined -> value is not assigned
+//symbol -> unique
+
+//object type:
+// type of
+
+// null is an object
+// undefined is undefined type
+
+console.log(typeof(age));
+console.log(typeof name);
